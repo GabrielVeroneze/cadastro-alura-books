a546f16bcae7c77258b5a78f55cb6931b5e703a8
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2023-02-17 23:05:39 -0300

    ::test_tube: test: Manipula erros da busca do cep

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,11 @@
 var consultaCep = fetch('https://viacep.com.br/ws/01001000/json/')
     .then(resposta => resposta.json())
-    .then(dados => console.log(dados))
+    .then(dados => {
+        if (dados.erro) {
+            throw Error('Esse cep não existe!')
+        } else {
+            console.log(dados)
+        }
+    })
+    .catch(erro => console.log(erro))
+
