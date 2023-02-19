fd20cc9250dcab3d9881c852710194571af2d99f
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2023-02-18 22:09:30 -0300

    :sparkles: feat: Manipula erro e realização do CEP

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,10 +2,10 @@ var consultaCep = fetch('https://viacep.com.br/ws/01001000/json/')
     .then(resposta => resposta.json())
     .then(dados => {
         if (dados.erro) {
-            throw Error('Esse cep não existe!')
+            throw Error('Esse CEP não existe!')
         } else {
             console.log(dados)
         }
     })
     .catch(erro => console.log(erro))
-
+    .finally(mensagem => console.log('Processamento concluído!'))
