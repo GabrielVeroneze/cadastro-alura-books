0d4785542fb6896b0d74fd81062416436247c378
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2023-02-19 22:54:47 -0300

    :test_tube: test: Busca CEP com função assíncrona

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,8 @@
-var consultaCep = fetch('https://viacep.com.br/ws/01001000/json/')
-    .then(resposta => resposta.json())
-    .then(dados => {
-        if (dados.erro) {
-            throw Error('Esse CEP não existe!')
-        } else {
-            console.log(dados)
-        }
-    })
-    .catch(erro => console.log(erro))
-    .finally(mensagem => console.log('Processamento concluído!'))
+async function buscaEndereco() {
+    var consultaCep = await fetch('https://viacep.com.br/ws/01001000/json/')
+    var consultaCepConvertido = await consultaCep.json()
+    console.log(consultaCep)
+    console.log(consultaCepConvertido)
+}
+
+buscaEndereco()
