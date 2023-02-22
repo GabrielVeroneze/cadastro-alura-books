e30e590f304aa8422f51f53b98c038159908ceac
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2023-02-21 22:57:09 -0300

    :test_tube: test: Cria novo teste

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,24 @@
-async function buscaEndereco() {
+async function buscaEndereco(cep) {
     try {
-        var consultaCep = await fetch('https://viacep.com.br/ws/01001001/json/')
+        var consultaCep = await fetch(`https://viacep.com.br/ws/${cep}/json/`)
         var cepConvertido = await consultaCep.json()
 
         if(cepConvertido.erro) {
             throw Error('CEP não existente!')
         }
-        console.log(cepConvertido)
+        // console.log(cepConvertido)
+        return cepConvertido
     } catch (erro) {
-        console.log(erro)
+        // console.log(erro)
+        return erro
     }
 }
 
-buscaEndereco()
+
+let listaCep = ['01001000', '01001001', '99999999', '111111111']
+
+let resultadoCep = listaCep.map(cep => {
+    return buscaEndereco(cep)
+})
+
+Promise.all(resultadoCep).then(respostas => console.log(respostas))
