6ae56e5c775c11e88a567e99ee5b9fe1d0ae4fec
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2023-02-23 23:14:05 -0300

    :test_tube: test: Cria novo teste

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,20 @@
+const cep = document.querySelector('#cep')
+var endereco = document.querySelector('#endereco')
+var numero = document.querySelector('#numero')
+var complemento = document.querySelector('#complemento')
+var bairro = document.querySelector('#bairro')
+var cidade = document.querySelector('#cidade')
+var estado = document.querySelector('#estado')
+
+
+cep.addEventListener('focusout', async () => {
+
+    const dados = await buscaEndereco(cep.value)
+
+    manipulaDados(dados)
+})
+
+
 async function buscaEndereco(cep) {
     try {
         var consultaCep = await fetch(`https://viacep.com.br/ws/${cep}/json/`)
@@ -7,9 +24,30 @@ async function buscaEndereco(cep) {
             throw Error('CEP não existente!')
         }
         // console.log(cepConvertido)
+        
         return cepConvertido
     } catch (erro) {
         // console.log(erro)
         return erro
     }
 }
+
+
+function manipulaDados(dados) {
+    console.log(dados)
+    endereco.value = dados.logradouro
+
+    complemento.value = dados.complemento
+    bairro.value = dados.bairro
+    cidade.value = dados.localidade
+}
+
+
+
+
+// console.log(endereco)
+// console.log(numero)
+// console.log(complemento)
+// console.log(bairro)
+// console.log(cidade)
+// console.log(estado)
